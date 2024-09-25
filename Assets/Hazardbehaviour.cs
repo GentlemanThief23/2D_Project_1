ff683566b8d0850f9ce9bdcc37567da8962c9daf
Brando Green <brandeman12@gmail.com>
2024-09-25 16:56:57 -0400

    EnemyFollow made

--- Assets/Hazardbehaviour.cs
+++ Assets/Hazardbehaviour.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 
 public class Hazardbehaviour : MonoBehaviour
 {
-    public GameObject Hazard
-        ;
+    public GameObject Hazard;
 
     public Transform LeftWall;
     public Transform RightWall;
@@ -18,7 +17,7 @@ public class Hazardbehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawn", 1, 1);
+        InvokeRepeating("Spawn", 1, .7f);
 
     }
 
@@ -34,7 +33,7 @@ public class Hazardbehaviour : MonoBehaviour
         int xpos = (int)Random.Range(LeftWall.position.x, RightWall.position.x);
         int ypos = (int)Random.Range(Ceiling.position.y, Ground.position.y);
 
-        Instantiate(Hazard, new Vector3(xpos, ypos, 0), Quaternion.identity);
-
+        GameObject temp = Instantiate(Hazard, new Vector3(xpos, ypos, 0), Quaternion.identity);
+        //temp.GetComponent<enemyFollow>().targetPlayer = GameObject.FindGameObjectWithTag("Player");
     }
 }
